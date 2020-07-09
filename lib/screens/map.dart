ddd557f65776a330c4fe4613ef3ee232652d3e27
chathu89916 <chathuracha@gmail.com>
2020-07-09 20:49:43 +0530

    map added

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -3,6 +3,7 @@ import 'package:Land_Finder/screens/lands.dart';
 import 'package:Land_Finder/style/appBarStyle.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapShow extends StatefulWidget {
   @override
@@ -10,6 +11,21 @@ class MapShow extends StatefulWidget {
 }
 
 class _MapShowState extends State<MapShow> {
+  bool mapToggle = false;
+  var currentLocation;
+
+  GoogleMapController _mapController;
+
+  void initState() {
+    super.initState();
+    Geolocator().getCurrentPosition().then((currloc) {
+      setState(() {
+        currentLocation = currloc;
+        mapToggle = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,13 +43,25 @@ class _MapShowState extends State<MapShow> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(6.9271, 79.8612),
-            zoom: 12.0,
-          ),
-        ),
+        child: mapToggle
+            ? GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(
+                      currentLocation.latitude, currentLocation.longitude),
+                  zoom: 12.0,
+                ),
+              )
+            : Center(
+                child: Text('Loading...'),
+              ),
       ),
     );
+  }
+
+  void _onMapCreated(controller) {
+    setState(() {
+      _mapController = controller;
+    });
   }
 }
